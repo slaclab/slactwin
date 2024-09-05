f3ffba076dc08c1093f34891c0878089473f8f84
Evan Carlin <evan@carlin.com>
2024-09-05 00:23:33 +0000

    deploy slactwin

--- k8s/alpha/job-supervisor-cmd.sh
+++ k8s/alpha/job-supervisor-cmd.sh
@@ -7,6 +7,16 @@ eval export HOME=~$USER
 set -euo pipefail
 cd '/srv/sirepo_job_supervisor'
 source ./env
+cd /tmp
+p=
+for r in radiasoft/sirepo radiasoft/pykern slaclab/slactwin; do
+    git clone -q -c advice.detachedHead=false --depth=1 https://github.com/"$r"
+    p+="$PWD/${r#*/}:"
+done
+export PYTHONPATH=$p
+# TODO(e-carlin): https://github.com/radiasoft/sirepo/issues/7238
+echo '__version__ = "1.1" # TODO(e-carlin): https://github.com/radiasoft/sirepo/issues/7238' > slactwin/slactwin/__init__.py
+cd -
 if [[ $@ ]]; then
     exec "$@"
 fi
