b52f23fb8c79f4e8d876f2db4eb69dbb1f5f17ac
robnagler <5495179+robnagler@users.noreply.github.com>
2024-09-10 20:37:42 +0000

    test not working

--- slactwin/pkcli/slactwin.py
+++ slactwin/pkcli/slactwin.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 """Wrapper to run from the command line.
 
 :copyright: Copyright (c) 2024 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
-:license: See LICENSE file for details.
+:license: http://github.com/slaclab/slactwin/LICENSE
 """
 
 
